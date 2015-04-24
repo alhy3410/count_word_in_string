5d437b70599d436dbdba1d791b53990b9e82d10b
Andrew Lee <andrewlhy@gmail.com>
2015-04-24 09:21:08 -0700

    added second spec to test to show a string

--- spec/count_word_in_string_spec.rb
+++ spec/count_word_in_string_spec.rb
@@ -6,4 +6,9 @@ describe('String#count_word_in_string') do
   it('allows the user to input a string and get that string as a result') do
     expect(('this').count_word_in_string()).to(eq('this'))
   end
+
+  it('allows the user to input a word as well as a string') do
+    expect(('this').count_word_in_string()).to(eq('oh very happy happy day'))
+  end
+  
 end
