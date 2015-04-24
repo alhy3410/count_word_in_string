0c73811774e79d1765596e390e2236bcedc3497a
Andrew Lee <andrewlhy@gmail.com>
2015-04-24 09:49:52 -0700

    set up the source plus the instructions for where to get each piece of information

---
+++ app.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/count_word_in_string')
+
+get('/') do
+  erb(:index)
+end
+
+get('/results')
+  @wordcount = params.fetch('test_word').count_word_in_string(params.fetch('word_list'))
+  erb(:results)
+end
