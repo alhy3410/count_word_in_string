a6ff3498eccd83347e157cde33d7c88d5443ddda
Andrew Lee <andrewlhy@gmail.com>
2015-04-24 09:18:43 -0700

    testing of the first spec works

---
+++ lib/count_word_in_string.rb
@@ -0,0 +1,6 @@
+class String
+  define_method(:count_word_in_string) do #|compared_words|
+    test_word = self
+    test_word
+  end
+end
--- spec/count_word_in_string_spec.rb
+++ spec/count_word_in_string_spec.rb
@@ -3,8 +3,7 @@ require('pry')
 require('count_word_in_string')
 
 describe('String#count_word_in_string') do
-  it('allows the user to input a string, and get that string as a result') do
-    expect('This is the string').count_word_in_string().to(eq("This is the string"))
+  it('allows the user to input a string and get that string as a result') do
+    expect(('this').count_word_in_string()).to(eq('this'))
   end
 end
-  
